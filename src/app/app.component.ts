cd571c7247bd5178620b3aa1dd87470336d5544a
mruiz <mruiz@rmcuantica.com>
2024-04-19 10:50:54 -0300

    Se modifica version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  version:string = '1'
+  version:string = '1.0.0'
   title = 'MR_Goalkeepers_Rastreo_Pats';
   esForm: boolean = false;
 
